9c23e47657a19eaa4d1083d92452e9c7cde742ff
Taha dlrb <tahadlrb7@gmail.com>
2023-06-01 16:10:26 +0330

    Add home blog flip flop cards

---
+++ components/Post/HomeFlipCard.vue
@@ -0,0 +1,157 @@
+<script setup lang="ts">
+import type { Post } from '~/utils/types'
+
+const { post } = defineProps<{
+  post: Post
+}>()
+
+const { public: envs } = useRuntimeConfig();
+
+const postBrief = computed(() => {
+  if (post.brief) return post.brief
+  else if (!post.brief && post.content) {
+    let content = post.content.replace(/\<img.*?\>/gi, '')
+    return content
+  }
+  return 'برای مشاهده پست روی دکمه زیر کلیک کنید.'
+})
+
+
+</script>
+
+<template>
+  <GlobalCardFlip :frontImg="post.image.url || ''" :backImg="post.image.url || ''">
+    <template #front>
+      <div class="bg-gray-900/20 rounded-md backdrop-filter backdrop-blur-sm p-5">
+        <p class="font-bold text-2xl">{{ post?.title }}</p>
+      </div>
+    </template>
+    <template #back>
+      <div
+        class="relative overflow-hidden pt-14 bg-gray-900/20 h-[calc(100%-100px)] w-[calc(100%-10px)] mt-3 rounded-lg backdrop-filter backdrop-blur-sm">
+        <span v-if="post.category.id" class="text-xs absolute top-2 right-2 bg-gray-50/30 py-1 px-3 rounded-full">
+          {{
+            post.category.name
+          }}
+        </span>
+
+        <div class="contents">
+          <p class="font-bold text-lg text-white px-2">{{ post.title }}</p>
+          <div v-html="postBrief" class="text-sm text-gray-200 text-justify pt-1 px-3 post-description">
+          </div>
+        </div>
+      </div>
+    </template>
+    <template #back-footer>
+      <p class="text-sm">برای مشاهده پست کلیک کنید</p>
+      <NuxtLink :to="{ name: 'posts-id', params: { id: post?.id } }" class="btn btn-primary btn-light mr-auto">
+        مشاهده</NuxtLink>
+    </template>
+  </GlobalCardFlip>
+</template>
+
+<style scoped>
+.card {
+  overflow: visible;
+  width: 100%;
+  height: 400px;
+}
+
+.content {
+  width: 100%;
+  height: 100%;
+  transform-style: preserve-3d;
+  transform: perspective(500px);
+  transition: transform 700ms;
+  border-radius: 5px;
+}
+
+.front,
+.back {
+  background-color: #151515;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+  -webkit-backface-visibility: hidden;
+  border-radius: 5px;
+  overflow: hidden;
+}
+
+.back {
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  overflow: hidden;
+
+}
+
+.back::before {
+  position: absolute;
+  content: ' ';
+  display: block;
+  width: 160px;
+  height: 160%;
+  background: linear-gradient(90deg, transparent, #ffffffb0, #ffffffb0, #ffffffb0, #ffffffb0, transparent);
+  animation: rotation_481 8s infinite linear;
+}
+
+.back-content {
+  position: absolute;
+  width: 99%;
+  height: 99%;
+  background-color: #151515;
+  background-size: cover;
+  border-radius: 5px;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+}
+
+.card:hover .content {
+  transform: rotateY(180deg);
+}
+
+@keyframes rotation_481 {
+  0% {
+    transform: rotateZ(0deg);
+  }
+
+  0% {
+    transform: rotateZ(360deg);
+  }
+}
+
+.front {
+  transform: rotateY(180deg);
+  color: white;
+}
+
+.front .front-content {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
+.front .bg-img {
+  background-size: cover;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  inset: 0;
+  transform: scale(1.2);
+}
+
+.post-description {
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 8;
+  -webkit-box-orient: vertical;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,33 @@
 <script setup lang="ts">
-const { addNotification } = useNotification()
+import type { Post } from '~/utils/types'
+
+const { find } = useStrapi();
+let posts: Post[] = await find('posts', {
+  populate: ['comments', 'image', 'category'],
+  fields: ['image', 'content', 'title', 'category', 'brief'],
+  pagination: { pageSize: 4 }
+})
+
 </script>
 <template>
-  <div class="bg-gray-900">
+  <div class="dark:bg-gray-900 pb-10">
     <BaseHero class="h-[calc(100vh-4rem)]" />
-    <div class="container bg-gray-900 mx-auto text-white mt-10">
+    <div class="container bg-gray-900 mx-auto text-white my-10">
       <div id="top-posts">
-        <h2 class="font-bold text-4xl p-5 pb-7 border-b-2 border-gray-100/40 mb-10">پست های پربازدید</h2>
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 lg:gap-16 p-7 lg:p-0">
-          <GlobalCardFlip v-for="card in 4" class="basis-3/12" title="شهر کبک کانادا"
-            content="کبک‌سیتی مرکز استان کبک کاناداست که میزبان متقاضیان مهاجرت به کانادا شده است. شهر کبک کانادا هم‌نام بزرگ‌ترین استان این کشور، موسسات آموزش عالی متعددی دارد و از این طریق میزبان علاقمندان به تحصیل در کانادا هم بوده است. برای آشنایی بیشتر با شرایط زندگی در کبک همراه ما در سایت موسسه مهاجرتی نیلگام باشید."
-            category="متفرقه" />
+        <h2 class="font-bold text-4xl p-5 pb-7 border-b-2 border-gray-100/40 mb-10">
+          پست های پربازدید
+        </h2>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 lg:gap-16 p-7 lg:py-7">
+          <PostHomeFlipCard v-for="post in posts" class="basis-3/12" title="شهر کبک کانادا" :post="post" />
         </div>
       </div>
     </div>
-    <div>
-    </div>
+    <div></div>
   </div>
 </template>
 
 <style scoped>
 .test-styles {
-  @apply text-sky-600 bg-sky-100
+  @apply text-sky-600 bg-sky-100;
 }
 </style>
