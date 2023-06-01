3729906a5c60904ac77d41da3d91ac933059b2aa
Taha dlrb <tahadlrb7@gmail.com>
2023-06-01 16:10:51 +0330

    useNotification composable improvement

--- composables/useNotification.ts
+++ composables/useNotification.ts
@@ -1,21 +1,24 @@
+import type { NOTIFICATION_TYPES, Notification } from '~/utils/types';
 import { generateRandUnique } from '~/utils';
-
-interface Notification {
-	id: string;
-	title?: string;
-	msg: string;
-}
 
 export const useNotification = () => {
 	const notifications = useState<Notification[]>('notifications', () => []);
-	function addNotification(msg: string, title: string, timeout: number = 1000) {
+
+	function addNotification(
+		msg: string,
+		type: NOTIFICATION_TYPES,
+		title: string = '',
+		timeout: number | false = false,
+	) {
 		const id = generateRandUnique(10);
-		const newNotification: Notification = { msg, id, title };
+		const newNotification: Notification = { msg, type, id, title };
 		notifications.value.push(newNotification);
 
-		setTimeout(() => {
-			removeNotification(id);
-		}, timeout);
+		if (timeout) {
+			setTimeout(() => {
+				removeNotification(id);
+			}, timeout);
+		}
 	}
 	function removeNotification(id: string) {
 		const notificationIndex = notifications.value.findIndex((n) => n.id === id);
