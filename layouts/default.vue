83ad82f0cfbda690b13a7f363fb4cf4861fbdd13
Taha dlrb <tahadlrb7@gmail.com>
2023-06-07 00:47:16 +0330

    Fixed navbar on top

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,29 @@
 <script setup lang="ts">
+useHead({
+  bodyAttrs: {
+    class: ['dark:bg-gray-900', 'dark:text-gray-50']
+  }
+})
 
+const userScrolled = ref<boolean>(false)
+onMounted(() => {
+  document.addEventListener('scroll', e => {
+    if (window.scrollY >= 50) {
+      userScrolled.value = true
+    } else {
+      userScrolled.value = false
+    }
+  });
+})
 </script>
 
 <template>
   <div>
     <header>
-      <Navbar />
+      <Navbar class="fixed left-0 top-0 right-0 z-50 transition-[height]"
+        :class="[userScrolled ? 'bg-opacity-10 backdrop-blur-md' : 'h-24']" />
     </header>
-    <main class="wrapper">
+    <main class="wrapper mt-24">
       <slot />
       <BaseNotification />
     </main>
