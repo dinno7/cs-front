20b28d781d5b0a13e361468cf00ae7ae26d46e1d
Taha dlrb <tahadlrb7@gmail.com>
2023-06-07 00:46:03 +0330

    Add author component

---
+++ components/Post/Author.vue
@@ -0,0 +1,36 @@
+
+<script setup lang="ts">
+// >> Imports
+
+import { User } from "~/utils/types"
+
+// >> Defines
+const { author } = defineProps<{
+  author: User
+}>()
+
+// >> Hooks
+
+// >> Data
+
+// >> Composable
+
+// >> Computed
+const authorAvatar = computed(() => {
+  const avatarUrl = author?.avatar?.url
+  if (avatarUrl) return normalizedStrapiImgSrcs(avatarUrl)
+  return '/images/user-photo.png'
+})
+
+// >> Functions
+
+</script>
+<template>
+  <div class="flex gap-2 items-center justify-start ">
+    <img class="w-[45px] h-[45px] aspect-square object-cover rounded-full" :src="authorAvatar" alt="Author">
+    <div class="flex flex-col text-sm dark:text-gray-200">
+      <p class="font-bold">{{ author.firstName + ' ' + author.lastName }}</p>
+      <p>{{ author.field }}</p>
+    </div>
+  </div>
+</template>
