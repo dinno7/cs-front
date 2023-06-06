22392ca1093c4fe72c2ac3689465ffe69dcb5cf3
Taha dlrb <tahadlrb7@gmail.com>
2023-06-07 00:45:28 +0330

    Loading component

---
+++ components/Global/Loading.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-col items-center justify-center gap-5">
+    <div
+      class="inline-block h-10 w-10 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-blue-400 motion-reduce:animate-[spin_1.5s_linear_infinite]"
+      role="status">
+    </div>
+    <span class="text-xl font-bold">درحال بارگزاری...</span>
+  </div>
+</template>
